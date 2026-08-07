3fe70d65fb732aec8abad7066b3290120377567f
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-08-07 20:20:53 +0530

    javascript

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,46 +1,79 @@
-console.log("strike is coming")
-// variable ko kaise banate h 
+console.log("Strike is coming");
 
-let name ="PRACHI";
-let age ="20";
-age="30";
+// Variable declaration
 
-console.log(name,age);
-const account =1234;
-// account =23;
+let name = "PRACHI";
+let age = 20;
+
+age = 30;
+
+console.log(name, age);
+
+const account = 1234;
+// account = 23; // Not allowed because account is a const
+
 console.log(account);
 
 // DATA TYPES
-//  primitive data type 
-// number,string ,boolean ,undefined ,null,bignit ,symbol
+// Primitive Data Types:
+// Number, String, Boolean, Undefined, Null, BigInt, Symbol
 
-// number
-let a= "10";
-let b="2.36";
-console.log(a,b);
+// Number
+let a = 10;
+let b = 2.36;
+console.log(a, b);
+
 // String
-let c=" strike rate is coming";
-let d='Anjali';
-console.log(c,d);
+let c = "Strike rate is coming";
+let d = "Anjali";
+console.log(c, d);
 
-let login = true ;
-let f=false;
-console.log(login,f)
+// Boolean
+let login = true;
+let f = false;
+console.log(login, f);
 
-//undefined( jab hum koi variable declare karenge and uske andar koi no. nhi daalenge then that is undefined)
-let user ;
-console .log(user);
+// Undefined
+// When a variable is declared but not assigned a value
+let user;
+console.log(user);
 
-// bigint
+// BigInt
 let num = 123456789n;
 console.log(num);
 
-// NULL
-let weather =current_weather("Dwarka");
-// 25
-// null
+// Null
+// Example: Weather data is not available yet
+let weather = null;
+console.log(weather);
+console.log(typeof weather);
 
+// Symbol
+const id1 = Symbol("id");
+console.log(id1);
 
+// Non-Primitive Data Types
+// Array, Object, Function
 
+// Array
+let arr = [10, 20, "prachi", true];
+console.log(arr);
 
-// Non-Primitive Data Type
+// Object: Stores data in key-value pairs.
+let obj = {
+    name: "rohit",
+    account: 1234,
+    age: 18,
+    category: "gen"
+};
+
+console.log(obj);
+
+// Function
+let s = function add() {
+    console.log("hello");
+};
+
+s(); // Calling the function
+
+console.log(typeof s); // function
