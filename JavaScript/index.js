74df4bca30060ba082284f49a26496b30fd1dbdf
pandeyprachi0172 <pandeyprachi0172@gmail.com>
2026-08-07 17:43:59 +0530

    inline css

---
+++ JavaScript/index.js
@@ -0,0 +1,46 @@
+console.log("strike is coming")
+// variable ko kaise banate h 
+
+let name ="PRACHI";
+let age ="20";
+age="30";
+
+console.log(name,age);
+const account =1234;
+// account =23;
+console.log(account);
+
+// DATA TYPES
+//  primitive data type 
+// number,string ,boolean ,undefined ,null,bignit ,symbol
+
+// number
+let a= "10";
+let b="2.36";
+console.log(a,b);
+// String
+let c=" strike rate is coming";
+let d='Anjali';
+console.log(c,d);
+
+let login = true ;
+let f=false;
+console.log(login,f)
+
+//undefined( jab hum koi variable declare karenge and uske andar koi no. nhi daalenge then that is undefined)
+let user ;
+console .log(user);
+
+// bigint
+let num = 123456789n;
+console.log(num);
+
+// NULL
+let weather =current_weather("Dwarka");
+// 25
+// null
+
+
+
+
+// Non-Primitive Data Type
